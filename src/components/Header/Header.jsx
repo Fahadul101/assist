56163beaf601b6c61374a809157a7b6c425ae193
Fahadul Islam <fahadulislam.user@outlook.com>
2022-01-06 01:59:27 +0600

    Create a React app and publish it to GitHub Pages

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './styles.js';
+
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor From Green Travel
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
